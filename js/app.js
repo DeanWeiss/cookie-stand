761d0645355ee2aeba799babf49e2ff5d88a1c14
Dean William Weiss <dean.w.weiss@gmail.com>
2022-04-12 10:14:57 -0700

    Added code to all the files

--- js/app.js
+++ js/app.js
@@ -1,20 +1,57 @@
-`use strict`;
+`use strict`
+/*
+Cookie Stand Object will have ....
+{
+    title: "The City/Location",
+    minCustomers: int,
+    maxCustomers: int,
+    avgCookiesPerCust: float,
+
+    generateRandonCustomersPerHour: function/method
+    randomCustomersPerHour: array of 15ish items
+}
+*/
+const hours = [`6am`, `7am`, `8am`, `9am`, `10am`, `11am`, `12pm`, `1pm`, `2pm`, `3pm`, `4pm`, `5pm`, `6pm`, `7pm`]
 
 const seattle = {
-location: `Seattle`,
-customer: 0,
-avgsale: 6.3,
-getCust: function (){
-    console.log(this.location);
-    this.customer = randomcustomer(23, 65) + ` Customers`;
-    console.log(this.customer)
+    title: "Seattle",
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgCookiePerCustomer: 6.3,
+    randomCookiesPerHour: [],
+    generateRandomCookiesPerHour: function () {
+        //generate values to populate randomCookiesPerHour
+        // array should have slots for 6am through 7pm (and maybe totals)
+        for (let i = 0; i < hours.length; i++){
+            const randomCustomersPerHour = randomInRange(this.minCustomers, this.maxCustomers);
+            this.randomCookiesPerHour[i] = Math.ceil(this.avgCookiePerCustomer * randomCustomersPerHour);
+        }
     }
-}
+};
 
-seattle.getCust();
+seattle.generateRandomCookiesPerHour();
 
-console.log(seattle);
+const lima = {
+    title: "Lima",
+    minCustomers: 2,
+    maxCustomers: 16,
+    avgCookiePerCustomer: 4.6,
+    randomCookiesPerHour: [],
+    generateRandomCookiesPerHour: function () {
+        //generate values to populate randomCookiesPerHour
+        // array should have slots for 6am through 7pm (and maybe totals)
+        for (let i = 0; i < hours.length; i++){
+            const randomCustomersPerHour = randomInRange(this.minCustomers, this.maxCustomers);
+            this.randomCookiesPerHour[i] = Math.ceil(this.avgCookiePerCustomer * randomCustomersPerHour);
+        }
+    }
+};
 
-function randomcustomer (min, max){
-    return math.floor(math.random() * (max - min + 1) + min);
+lima.generateRandomCookiesPerHour();
+
+function randomInRange(min,max){
+    const span = max - min + 1;
+    const randInSpan = Math.floor(Math.random() * span);
+    return min + randInSpan;
+    
 }
